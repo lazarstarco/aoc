1eb38aeae1a7f535d6f264e77eb4200198d0800e
lazarstarco <lazarstarco@gmail.com>
2024-12-10 11:22:18 +0100

    added 7.2, not working

--- 2024/07/2.js
+++ 2024/07/2.js
@@ -7,35 +7,41 @@ const operands = ["+", "*", "||"];
 
 const equations = input.replaceAll(": ", "=").split("\r\n");
 
-const makeCombinations = (equations) => {
-  let results = [];
+// const makeCombinations = (equations) => {
+//   let results = [];
 
-  equations.forEach((equation) => {
-    let queue = [equation];
+//   equations.forEach((equation) => {
+//     let queue = [equation];
 
-    while (queue.length > 0) {
-      let curr = queue.shift();
+//     while (queue.length > 0) {
+//       let curr = queue.shift();
 
-      if (curr.indexOf(" ") === -1) {
-        results.push(curr);
-      } else {
-        operands.forEach((operand) => {
-          queue.push(curr.replace(" ", operand));
-        });
-      }
-    }
-  });
+//       if (curr.indexOf(" ") === -1) {
+//         results.push(curr);
+//       } else {
+//         operands.forEach((operand) => {
+//           queue.push(curr.replace(" ", operand));
+//         });
+//       }
+//     }
+//   });
 
-  return results;
-};
+//   return results;
+// };
 
 const formatFormula = (formula) => {
   return formula.match(/(\d+|\+|\*|\|\|)/g);
 };
 
-const calculate = (equals, formula) => {
+const calculate = (combination, index, maximum) => {
+  const { equals, formula } = combination;
+  console.log(`Processing ${index}/${maximum}: ${JSON.stringify(combination)}`);
+
   let result = +formula[0];
   for (let i = 1; i < formula.length - 1; i += 2) {
+    if (result > +equals) {
+      return false;
+    }
     switch (formula[i]) {
       case "+":
         result = result + +formula[i + 1];
@@ -52,18 +58,48 @@ const calculate = (equals, formula) => {
   return +equals === result;
 };
 
-const combinations = makeCombinations(equations).map((x) => ({
-  equals: x.split("=")[0],
-  formula: formatFormula(x.split("=")[1]),
-}));
+// const combinations = makeCombinations(equations);
+// const mappedCombinations = combinations.map((x) => ({
+//   equals: x.split("=")[0],
+//   formula: formatFormula(x.split("=")[1]),
+// }));
 
-const results = combinations
-  .filter((combination) => calculate(combination.equals, combination.formula))
-  .map((combination) => combination.equals);
+// const results = mappedCombinations
+//   .filter((combination) => calculate(combination))
+//   .map((combination) => combination.equals);
 
-console.log([...new Set(results)].reduce((aggr, curr) => aggr + +curr, 0));
+// console.log([...new Set(results)].reduce((aggr, curr) => aggr + +curr, 0));
+
+const resultSet = new Set();
+
+equations.forEach((equation, i) => {
+  let queue = [equation];
+
+  while (queue.length > 0) {
+    let curr = queue.shift();
+
+    if (curr.indexOf(" ") === -1) {
+      const formatted = {
+        equals: curr.split("=")[0],
+        formula: formatFormula(curr.split("=")[1]),
+      };
+      if (calculate(formatted, i + 1, equations.length)) {
+        resultSet.add(formatted.equals);
+        continue;
+      }
+    } else {
+      operands.forEach((operand) => {
+        queue.push(curr.replace(" ", operand));
+      });
+    }
+  }
+});
+
+console.log(resultSet.values().reduce((aggr, curr) => aggr + +curr, 0));
 
 const endTime = performance.now();
 console.log(
-  `Execution time of 2024/08/2.js took ${endTime - startTime} milliseconds`
+  `Execution time of ${process.argv[1].substring(
+    process.argv[1].indexOf("aoc")
+  )} took ${endTime - startTime} milliseconds`
 );
