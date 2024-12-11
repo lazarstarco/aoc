f9c6b91b12bdae9acfdb2e44dfb4edab4c2056e5
lazarstarco <lazarstarco@gmail.com>
2024-12-11 13:23:38 +0100

    fixed 7.2 and added 9.1

--- 2024/07/2.js
+++ 2024/07/2.js
@@ -1,105 +1,106 @@
 import { readFileSync } from "fs";
+import { performance } from "perf_hooks";
 
 const startTime = performance.now();
 const input = readFileSync("input.txt", "utf8");
 
 const operands = ["+", "*", "||"];
-
 const equations = input.replaceAll(": ", "=").split("\r\n");
-
-// const makeCombinations = (equations) => {
-//   let results = [];
-
-//   equations.forEach((equation) => {
-//     let queue = [equation];
-
-//     while (queue.length > 0) {
-//       let curr = queue.shift();
-
-//       if (curr.indexOf(" ") === -1) {
-//         results.push(curr);
-//       } else {
-//         operands.forEach((operand) => {
-//           queue.push(curr.replace(" ", operand));
-//         });
-//       }
-//     }
-//   });
-
-//   return results;
-// };
 
 const formatFormula = (formula) => {
   return formula.match(/(\d+|\+|\*|\|\|)/g);
 };
 
-const calculate = (combination, index, maximum) => {
+const calculate = (combination) => {
   const { equals, formula } = combination;
-  console.log(`Processing ${index}/${maximum}: ${JSON.stringify(combination)}`);
 
-  let result = +formula[0];
-  for (let i = 1; i < formula.length - 1; i += 2) {
-    if (result > +equals) {
+  let result = BigInt(formula[0]);
+
+  for (let i = 1; i < formula.length; i += 2) {
+    if (result > BigInt(equals)) {
       return false;
     }
     switch (formula[i]) {
       case "+":
-        result = result + +formula[i + 1];
+        result += BigInt(formula[i + 1]);
         break;
       case "*":
-        result = result * +formula[i + 1];
+        result *= BigInt(formula[i + 1]);
         break;
       case "||":
-        result = result + formula[i + 1];
-        result = +result;
+        result = BigInt(String(result) + formula[i + 1]);
         break;
     }
   }
-  return +equals === result;
+  return BigInt(equals) === result;
 };
 
-// const combinations = makeCombinations(equations);
-// const mappedCombinations = combinations.map((x) => ({
-//   equals: x.split("=")[0],
-//   formula: formatFormula(x.split("=")[1]),
-// }));
+const processBatch = async (batch, batchIndex, totalBatches) => {
+  console.log(`Batch ${batchIndex + 1}/${totalBatches} started`);
+  const resultSet = new Set();
 
-// const results = mappedCombinations
-//   .filter((combination) => calculate(combination))
-//   .map((combination) => combination.equals);
+  for (const equation of batch) {
+    let queue = [equation];
 
-// console.log([...new Set(results)].reduce((aggr, curr) => aggr + +curr, 0));
+    while (queue.length > 0) {
+      let curr = queue.shift();
 
-const resultSet = new Set();
-
-equations.forEach((equation, i) => {
-  let queue = [equation];
-
-  while (queue.length > 0) {
-    let curr = queue.shift();
-
-    if (curr.indexOf(" ") === -1) {
-      const formatted = {
-        equals: curr.split("=")[0],
-        formula: formatFormula(curr.split("=")[1]),
-      };
-      if (calculate(formatted, i + 1, equations.length)) {
-        resultSet.add(formatted.equals);
-        continue;
+      if (curr.indexOf(" ") === -1) {
+        const formatted = {
+          equals: curr.split("=")[0],
+          formula: formatFormula(curr.split("=")[1]),
+        };
+        if (calculate(formatted)) {
+          resultSet.add(formatted);
+          console.log(
+            `Value ${JSON.stringify(formatted)} added to the result set`
+          );
+        }
+      } else {
+        operands.forEach((operand) => {
+          queue.push(curr.replace(" ", operand));
+        });
       }
-    } else {
-      operands.forEach((operand) => {
-        queue.push(curr.replace(" ", operand));
-      });
     }
   }
-});
 
-console.log(resultSet.values().reduce((aggr, curr) => aggr + +curr, 0));
+  console.log(`Batch ${batchIndex + 1}/${totalBatches} completed`);
+  return resultSet;
+};
 
-const endTime = performance.now();
-console.log(
-  `Execution time of ${process.argv[1].substring(
-    process.argv[1].indexOf("aoc")
-  )} took ${endTime - startTime} milliseconds`
-);
+const chunkSize = 50;
+const batches = [];
+for (let i = 0; i < equations.length; i += chunkSize) {
+  batches.push(equations.slice(i, i + chunkSize));
+}
+
+const main = async () => {
+  const batchPromises = batches.map((batch, index) =>
+    processBatch(batch, index, batches.length)
+  );
+
+  const results = await Promise.all(batchPromises);
+
+  const combinedResults = new Set();
+  results.forEach((resultSet) => {
+    resultSet
+      .values()
+      .map((item) => ({
+        equals: item.equals,
+        formula: item.formula.filter((_, index) => index % 2 === 0),
+      }))
+      .forEach((item) => combinedResults.add(JSON.stringify(item)));
+  });
+
+  const finalSum = Array.from(combinedResults)
+    .map((item) => JSON.parse(item))
+    .reduce((aggr, curr) => aggr + BigInt(curr.equals), BigInt(0));
+  console.log(`Final Sum: ${finalSum.toString()}`);
+
+  const endTime = performance.now();
+  console.log(
+    `Execution time: ${(endTime - startTime).toFixed(2)} milliseconds`
+  );
+};
+
+main().catch((err) => console.error(err));
--- 2024/09/1.js
+++ 2024/09/1.js
@@ -3,7 +3,59 @@ import { readFileSync } from "fs";
 const startTime = performance.now();
 const input = readFileSync("input.txt", "utf8");
 
-// code
+const sequence = input.split("");
+
+const isFormatted = (arr) => {
+  return /^\d+\.+$/.test(arr.join(""));
+};
+
+const map = [];
+let mapIndex = 0;
+for (let i = 0; i < sequence.length; i++) {
+  for (let j = 0; j < sequence[i]; j++) {
+    if (i % 2 == 0) {
+      map.push(mapIndex.toString());
+    } else {
+      map.push(".");
+    }
+  }
+
+  if (i % 2 == 0) {
+    mapIndex++;
+  }
+}
+
+const getBeginning = () => {
+  for (let i = 0; i < map.length; i++) {
+    if (map[i] == ".") {
+      return i;
+    }
+  }
+  return map.length - 1;
+};
+
+const getEnd = () => {
+  for (let i = map.length - 1; i > 0; i--) {
+    if (map[i] != ".") {
+      return i;
+    }
+  }
+  return 0;
+};
+
+while (!isFormatted(map)) {
+  const beginning = getBeginning();
+  const end = getEnd();
+  const temp = map[beginning];
+  map[beginning] = map[end];
+  map[end] = temp;
+}
+
+console.log(
+  `Result: ${map
+    .map((x) => (x !== "." ? +x : 0))
+    .reduce((aggr, curr, i) => i * curr + aggr, 0)}`
+);
 
 const endTime = performance.now();
 console.log(
