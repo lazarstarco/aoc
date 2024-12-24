ed8e7363513805595201bf4aa0896ba913be81f7
Lazar Starcevic <lazar.starcevic@paysafe.com>
2024-12-24 16:40:11 +0100

    added day 11

--- 2024/11/1.js
+++ 2024/11/1.js
@@ -3,7 +3,37 @@ import { readFileSync } from "fs";
 const startTime = performance.now();
 const input = readFileSync("input.txt", "utf8");
 
-// code
+let blinks = 25;
+let stones = input.split(" ").map((stone) => +stone);
+console.log(stones);
+
+while (blinks-- > 0) {
+  const newStones = [];
+  for (let i = 0; i < stones.length; i++) {
+    if (stones[i] === 0) {
+      newStones.push(1);
+    } else if (stones[i].toString().length % 2 === 0) {
+      const firstHalf = +stones[i]
+        .toString()
+        .split("")
+        .splice(0, stones[i].toString().length / 2)
+        .join("");
+      const secondHalf = +stones[i]
+        .toString()
+        .split("")
+        .splice(stones[i].toString().length / 2)
+        .join("");
+      newStones.push(firstHalf);
+      newStones.push(secondHalf);
+    } else {
+      newStones.push(stones[i] * 2024);
+    }
+  }
+  stones = newStones;
+  console.log(stones);
+}
+
+console.log(`Result: ${stones.length}`);
 
 const endTime = performance.now();
 console.log(
--- 2024/11/2.js
+++ 2024/11/2.js
@@ -1,9 +1,40 @@
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 
 const startTime = performance.now();
 const input = readFileSync("input.txt", "utf8");
 
-// code
+let blinks = 75;
+let stones = input.split(" ").map((stone) => +stone);
+// console.log(stones);
+
+while (blinks-- > 0) {
+  const newStones = [];
+  for (let i = 0; i < stones.length; i++) {
+    if (stones[i] === 0) {
+      newStones.push(1);
+    } else if (stones[i].toString().length % 2 === 0) {
+      const firstHalf = +stones[i]
+        .toString()
+        .split("")
+        .splice(0, stones[i].toString().length / 2)
+        .join("");
+      const secondHalf = +stones[i]
+        .toString()
+        .split("")
+        .splice(stones[i].toString().length / 2)
+        .join("");
+      newStones.push(firstHalf);
+      newStones.push(secondHalf);
+    } else {
+      newStones.push(stones[i] * 2024);
+    }
+  }
+  writeFileSync("./output.txt", `${75 - blinks} blinks, ${stones.length} elements\n`);
+  stones = newStones;
+  // console.log(stones);
+}
+
+console.log(`Result: ${stones.length}`);
 
 const endTime = performance.now();
 console.log(
