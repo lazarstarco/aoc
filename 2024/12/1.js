4e8edc21835c420a0c1b7cd1770630bdb9cf5bda
Lazar Starcevic <lazar.starcevic@paysafe.com>
2024-12-24 15:51:31 +0100

    added 12.1

--- 2024/12/1.js
+++ 2024/12/1.js
@@ -3,120 +3,86 @@ import { readFileSync } from "fs";
 const startTime = performance.now();
 const input = readFileSync("input.txt", "utf8");
 
-const print = (fig) => {
-  const curr = Object.keys(fig).includes("figure") ? fig.figure : fig;
+const map = input.split(/\r?\n/).map((line) => line.split(""));
+const visited = Array.from({ length: map.length }, () =>
+  new Array(map[0].length).fill(false)
+);
 
-  curr.forEach((row) => {
-    row.forEach((col) => {
-      process.stdout.write((col === "." ? " " : col) + " ");
-    });
-    process.stdout.write("\n");
-  });
-  process.stdout.write("\n");
+const isAllVisited = () => {
+  return visited.every((row) => row.every((cell) => cell));
 };
 
-const countEdges = (fig, i, j, letter) => {
-  let count = 0;
-
-  const directions = [
-    [-1, 0],
-    [1, 0],
-    [0, -1],
-    [0, 1],
-  ];
-
-  const isOutOfBounds = (i, j) => {
-    return i < 0 || i >= fig.length || j < 0 || j >= fig[i].length;
+const analyzeCell = (i, j) => {
+  const calculateCell = (i, j) => {
+    let cellResult = 0;
+    cellResult += i - 1 < 0 || map[i - 1][j] !== map[i][j] ? 1 : 0;
+    cellResult += j - 1 < 0 || map[i][j - 1] !== map[i][j] ? 1 : 0;
+    cellResult += j + 1 >= map[0].length || map[i][j + 1] !== map[i][j] ? 1 : 0;
+    cellResult += i + 1 >= map.length || map[i + 1][j] !== map[i][j] ? 1 : 0;
+    return cellResult;
   };
 
-  directions.forEach(([di, dj]) => {
-    const ni = i + di,
-      nj = j + dj;
+  visited[i][j] = true;
 
-    if (isOutOfBounds(ni, nj)) {
-      count++;
-    } else if (fig[ni][nj] !== letter) {
-      count++;
+  let result = { count: 1, calc: calculateCell(i, j) };
+
+  if (i - 1 >= 0 && map[i - 1][j] === map[i][j] && !visited[i - 1][j]) {
+    const analysis = analyzeCell(i - 1, j);
+    result = {
+      count: result.count + analysis.count,
+      calc: result.calc + analysis.calc,
+    };
+  }
+  if (j - 1 >= 0 && map[i][j - 1] === map[i][j] && !visited[i][j - 1]) {
+    const analysis = analyzeCell(i, j - 1);
+    result = {
+      count: result.count + analysis.count,
+      calc: result.calc + analysis.calc,
+    };
+  }
+  if (
+    j + 1 < map[0].length &&
+    map[i][j + 1] === map[i][j] &&
+    !visited[i][j + 1]
+  ) {
+    const analysis = analyzeCell(i, j + 1);
+    result = {
+      count: result.count + analysis.count,
+      calc: result.calc + analysis.calc,
+    };
+  }
+  if (i + 1 < map.length && map[i + 1][j] === map[i][j] && !visited[i + 1][j]) {
+    const analysis = analyzeCell(i + 1, j);
+    result = {
+      count: result.count + analysis.count,
+      calc: result.calc + analysis.calc,
+    };
+  }
+
+  return result;
+};
+
+let i = 0;
+let j = 0;
+let finalResult = 0;
+while (!isAllVisited()) {
+  const finalAnalysis = analyzeCell(i, j);
+  finalResult += finalAnalysis.count * finalAnalysis.calc;
+  for (i = 0; i < map.length; i++) {
+    let foundUnvisited = false;
+    for (j = 0; j < map[0].length; j++) {
+      if (!visited[i][j]) {
+        foundUnvisited = true;
+        break;
+      }
     }
-  });
+    if (foundUnvisited) {
+      break;
+    }
+  }
+}
 
-  return count;
-};
-
-const formatFigure = (map, letter) => {
-  const figure = [...Array(map.length)].map(() => [Array(map.length).fill("")]);
-  let found = false;
-
-  map.forEach((row, i) =>
-    row.forEach((col, j) => {
-      if (col !== letter) {
-        figure[i][j] = ".";
-      }
-      if (col !== letter && found) {
-        return;
-      }
-      if (col === letter) {
-        found = true;
-        figure[i][j] = letter;
-      }
-    })
-  );
-
-  return figure;
-};
-
-const map = input.split(/\r?\n/).map((x) => x.split(""));
-const visited = [...Array(map.length)].map(() => [
-  Array(map.length).fill(false),
-]);
-let flatMap = map.flatMap((x) => x);
-
-print(map);
-
-const figures = [];
-let letter = undefined;
-do {
-  letter = flatMap.find((x) => x !== ".");
-  const figure = formatFigure(map, letter);
-
-  figures.push({ letter, figure });
-  flatMap = flatMap.filter((x) => x !== letter);
-} while (letter !== undefined);
-figures.splice(figures.length - 1);
-
-const results = [];
-
-figures.forEach((currentFigure) => {
-  const result =
-    currentFigure.figure
-      .flatMap((x) => x)
-      .filter((x) => x === currentFigure.letter).length *
-    currentFigure.figure
-      .map((row, i) =>
-        row.map((col, j) =>
-          col === currentFigure.letter
-            ? countEdges(currentFigure.figure, i, j, currentFigure.letter)
-            : 0
-        )
-      )
-      .flatMap((x) => x)
-      .reduce((aggr, curr) => aggr + curr, 0);
-  console.log(
-    "Letter:",
-    currentFigure.letter,
-    "Figure size:",
-    currentFigure.figure
-      .flatMap((x) => x)
-      .filter((x) => x === currentFigure.letter).length,
-    "Result: ",
-    result
-  );
-
-  print(currentFigure);
-  results.push(result);
-});
-
-console.log(`Final result: ${results.reduce((aggr, curr) => aggr + curr, 0)}`);
+console.log(finalResult);
 
 const endTime = performance.now();
 console.log(
